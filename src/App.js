bbf7fccb08ceba2bcefdae9837c2efe7e66a61c9
Suraj10061997 <surajsingh10061997@gmail.com>
2022-08-08 21:32:54 +0530

    changes done successfully

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  // const ops = ["+","-","/","*"]
   const [ans, setAns] = useState("");
 
   const HandleClick = (e) => {
@@ -14,41 +15,82 @@ function App() {
   }
 
 
-  /*
-
-  let x = 10;
-  let y = 20;
-  let text = "x * y";
-  let result = eval(text);
-  */
-
-  //The eval() executes an expression and return a value
+  const Add = (character) => {
+    let value = ans.split(character);
+    let num1 = Number(value[0]);
+    let num2 = Number(value[1]);
+    let result = num1 + num2;
+    setAns(result);
+  }
+  const Multiply = (character) => {
+    let value = ans.split(character);
+    let num1 = Number(value[0]);
+    let num2 = Number(value[1]);
+    let result = num1 * num2;
+    setAns(result);
+  }
+  const Division = (character) => {
+    let value = ans.split(character);
+    let num1 = Number(value[0]);
+    let num2 = Number(value[1]);
+    let result = num1 / num2;
+    setAns(result);
+  }
+  const Substract = (character) => {
+    let value = ans.split(character);
+    let num1 = Number(value[0]);
+    let num2 = Number(value[1]);
+    let result = num1 - num2;
+    setAns(result);
+  }
   const Calculate = () => {
-    setAns(eval(ans).toString())
+    let value = ans.split("");
+    if (value.includes("+") || value.includes("-") || value.includes("/") || value.includes("*")) {
+      for (let i = 0; i < value.length; i++) {
+        if (value[i] === "+" || value[i] === "*" || value[i] === "/" || value[i] === "-") {
+          let character = value[i];
+          if (character === "+") {
+            Add(character);
+          }
+          else if (character === "*") {
+            Multiply(character);
+          }
+          else if (character === "/") {
+            Division(character);
+          }
+          else if(character === "-") {
+            Substract(character);
+          }
+        }
+      }
+    }
+    else{
+      setAns("Error");
+    }
   }
   return (
     <div className="calculator">
       <h1 className='heading'>Calculator</h1>
       <div className='parent'>
-      <input tupe="text" placeholder='0' id="answer" className='button' value={ans} />
-      <input type="button" value="9" className='button' onClick={HandleClick} />
-      <input type="button" value="8" className='button' onClick={HandleClick} />
-      <input type="button" value="7" className='button' onClick={HandleClick} />
-      <input type="button" value="6" className='button' onClick={HandleClick} />
-      <input type="button" value="5" className='button' onClick={HandleClick} />
-      <input type="button" value="4" className='button' onClick={HandleClick} />
-      <input type="button" value="3" className='button' onClick={HandleClick} />
-      <input type="button" value="2" className='button' onClick={HandleClick} />
-      <input type="button" value="1" className='button' onClick={HandleClick} />
-      <input type="button" value="0" className='button' onClick={HandleClick} />
-      <input type="button" value="." className='button' onClick={HandleClick} />
-      <input type="button" value="+" className='button' onClick={HandleClick} />
-      <input type="button" value="-" className='button' onClick={HandleClick} />
-      <input type="button" value="*" className='button' onClick={HandleClick} />
-      <input type="button" value="/" className='button' onClick={HandleClick} />
-      <input type="button" value="%" className='button' onClick={HandleClick} />
-      <input type="button" value="C" className='button' onClick={ClearAns} />
-      <input type="button" value="=" className='button' onClick={Calculate} />
+        <input tupe="text" placeholder='0' id="answer" className='button' value={ans} />
+        <input type="button" value="9" className='button' onClick={HandleClick} />
+        <input type="button" value="8" className='button' onClick={HandleClick} />
+        <input type="button" value="7" className='button' onClick={HandleClick} />
+        <input type="button" value="6" className='button' onClick={HandleClick} />
+        <input type="button" value="5" className='button' onClick={HandleClick} />
+        <input type="button" value="4" className='button' onClick={HandleClick} />
+        <input type="button" value="3" className='button' onClick={HandleClick} />
+        <input type="button" value="2" className='button' onClick={HandleClick} />
+        <input type="button" value="1" className='button' onClick={HandleClick} />
+        <input type="button" value="0" className='button' onClick={HandleClick} />
+        <input type="button" value="." className='button' onClick={HandleClick} />
+        <input type="button" value="+" className='button' onClick={HandleClick} />
+        <input type="button" value="-" className='button' onClick={HandleClick} />
+        <input type="button" value="*" className='button' onClick={HandleClick} />
+        <input type="button" value="/" className='button' onClick={HandleClick} />
+        <input type="button" value="%" className='button' onClick={HandleClick} />
+        <input type="button" value="C" className='button' onClick={ClearAns} />
+        <input type="button" value="=" className='button' onClick={Calculate} />
       </div>
     </div>
   );
