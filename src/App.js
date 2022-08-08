b3676988a1897fa117936dad795332e6e023bea3
Suraj10061997 <surajsingh10061997@gmail.com>
2022-08-08 20:45:18 +0530

    final changes done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [ans, setAns] = useState("");
+
+  const HandleClick = (e) => {
+    setAns(ans.concat(e.target.value))
+  }
+
+  const ClearAns = () => {
+    setAns("");
+  }
+
+
+  /*
+
+  let x = 10;
+  let y = 20;
+  let text = "x * y";
+  let result = eval(text);
+  */
+
+  //The eval() executes an expression and return a value
+  const Calculate = () => {
+    setAns(eval(ans).toString())
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <h1 className='heading'>Calculator</h1>
+      <div className='parent'>
+      <input tupe="text" placeholder='0' id="answer" className='button' value={ans} />
+      <input type="button" value="9" className='button' onClick={HandleClick} />
+      <input type="button" value="8" className='button' onClick={HandleClick} />
+      <input type="button" value="7" className='button' onClick={HandleClick} />
+      <input type="button" value="6" className='button' onClick={HandleClick} />
+      <input type="button" value="5" className='button' onClick={HandleClick} />
+      <input type="button" value="4" className='button' onClick={HandleClick} />
+      <input type="button" value="3" className='button' onClick={HandleClick} />
+      <input type="button" value="2" className='button' onClick={HandleClick} />
+      <input type="button" value="1" className='button' onClick={HandleClick} />
+      <input type="button" value="0" className='button' onClick={HandleClick} />
+      <input type="button" value="." className='button' onClick={HandleClick} />
+      <input type="button" value="+" className='button' onClick={HandleClick} />
+      <input type="button" value="-" className='button' onClick={HandleClick} />
+      <input type="button" value="*" className='button' onClick={HandleClick} />
+      <input type="button" value="/" className='button' onClick={HandleClick} />
+      <input type="button" value="%" className='button' onClick={HandleClick} />
+      <input type="button" value="C" className='button' onClick={ClearAns} />
+      <input type="button" value="=" className='button' onClick={Calculate} />
+      </div>
     </div>
   );
 }
